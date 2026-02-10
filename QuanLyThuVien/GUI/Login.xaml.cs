41eecf71c31895b0a4ecb0229a9973239f609525
Dung-binD <ledung19112003@gmail.com>
2026-02-10 12:18:38 +0700

    feat: Design UI Login + MainWindow

--- QuanLyThuVien/GUI/Login.xaml.cs
+++ QuanLyThuVien/GUI/Login.xaml.cs
@@ -14,14 +14,39 @@ using System.Windows.Shapes;
 
 namespace QuanLyThuVien.GUI
 {
-    /// <summary>
-    /// Interaction logic for Login.xaml
-    /// </summary>
     public partial class Login : Window
     {
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUser.Text.Trim();
+            string password = txtPass.Password;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!", "Thông báo");
+                return;
+            }
+
+            // Tài khoản test
+            if (username == "admin" && password == "123")
+            {
+                //MessageBox.Show("Đăng nhập thành công!");
+
+                // VÌ MainWindow nằm cùng thư mục GUI với Login, 
+                // bạn gọi trực tiếp mà không cần thêm using hay đường dẫn phức tạp.
+                MainWindow main = new MainWindow();
+                main.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Sai tài khoản hoặc mật khẩu!");
+            }
         }
     }
 }
